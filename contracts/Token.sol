42af08c417cfc476601feedac1b47da86e7f2880
rayedsikder <rayedsikder33@gmail.com>
2022-10-22 19:07:01 +0600

    Add token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(string memory name_, string memory symbol_)
+        ERC20(name_, symbol_)
+    {}
+}
