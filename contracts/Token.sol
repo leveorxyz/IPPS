a326f3030639a745ed3856f404ba51bcd9c999d7
rayedsikder <rayedsikder33@gmail.com>
2022-10-23 19:19:39 +0600

    Add initializer for ExchangeProtocol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MyToken is ERC20, ERC20Burnable, Ownable {
+contract Token is ERC20, ERC20Burnable, Ownable {
     constructor(string memory name_, string memory symbol_, address exchangeProtocol_)
         ERC20(name_, symbol_)
     {
--- contracts/TokenFactory.sol
+++ contracts/TokenFactory.sol
@@ -5,7 +5,12 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Token.sol";
 
 contract TokenFactory is Ownable {
+    address exchangeProtocol;
     mapping (string => address) tokenSymbolToAddress;
+
+    function initialize(address _exchangeProtocol) public onlyOwner {
+        exchangeProtocol = _exchangeProtocol;
+    }
 
     function getToken(string calldata name) public view returns (address) {
         return tokenSymbolToAddress[name];
@@ -13,7 +18,7 @@ contract TokenFactory is Ownable {
 
     function createToken(string calldata name, string calldata symbol) public onlyOwner{
         require(tokenSymbolToAddress[name] == address(0), "TokenFactory: Token already exists");
-        Token newToken = new Token(name, symbol);
+        Token newToken = new Token(name, symbol, exchangeProtocol);
         require(address(newToken) != address(0), "TokenFactory: Token deployment unsuccessfull");
         tokenSymbolToAddress[name] = address(newToken);
     }
