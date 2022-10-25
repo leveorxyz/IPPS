794efd68c2d38aa037aa3382846743cea7accfb7
rayedsikder <rayedsikder33@gmail.com>
2022-10-25 19:20:38 +0600

    Add transfer and access mechanisms in Token

--- contracts/ExchangeProtocol.sol
+++ contracts/ExchangeProtocol.sol
@@ -2,20 +2,42 @@
 pragma solidity 0.8.17;
 
 import "./TokenFactory.sol";
+import "./Token.sol";
 import "./UserRegistry.sol";
 import "./Oracle.sol";
 
 contract ExchangeProtocol {
     address userRegistry;
     address oracleAdddress;
-    
-    modifier isWhitelisted() {
-        require(UserRegistry(userRegistry).getWhitelistStatus(msg.sender), "ExchangeProtocol: User not whitelisted");
+
+    modifier isWhitelisted(address dstReceiver) {
+        require(
+            UserRegistry(userRegistry).getWhitelistStatus(msg.sender) &&
+                UserRegistry(userRegistry).getWhitelistStatus(dstReceiver),
+            "ExchangeProtocol: User not whitelisted"
+        );
         _;
     }
 
-    function transferToken(address srcToken, address dstToken, address dstReceiver, uint256 dstAmount) public isWhitelisted {
-        uint256 srcValue = Oracle(oracleAdddress).getAssetPriceInUSD(srcToken);
-        uint256 dstValue = Oracle(oracleAdddress).getAssetPriceInUSD(dstToken);
+    function transferToken(
+        address srcToken,
+        address dstToken,
+        address dstReceiver,
+        uint256 dstAmount
+    ) public isWhitelisted(dstReceiver) {
+        if (srcToken == dstToken) {
+            Token(srcToken).transferFrom(msg.sender, dstReceiver, dstAmount);
+        } else {
+            uint256 srcValue = Oracle(oracleAdddress).getAssetPriceInUSD(
+                srcToken
+            );
+            uint256 dstValue = Oracle(oracleAdddress).getAssetPriceInUSD(
+                dstToken
+            );
+            uint256 srcAmount = (dstAmount * dstValue) / srcValue;
+
+            Token(srcToken).burnFrom(msg.sender, srcAmount);
+            Token(dstToken).mint(dstReceiver, dstAmount);
+        }
     }
 }
