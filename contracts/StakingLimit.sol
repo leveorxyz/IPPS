518f5cc684c7836c159ddd48195330d65238115a
rayedsikder <rayedsikder33@gmail.com>
2022-10-26 23:26:48 +0600

    Update data types and initialization

--- contracts/StakingLimit.sol
+++ contracts/StakingLimit.sol
@@ -3,22 +3,28 @@ pragma solidity 0.8.17;
 
 contract StakingLimit {
     struct Bank {
+        bytes32 bankName;
         bytes32 routingNumber;
         bool isRegistered;
         bytes bankAddress;
         bytes url;
-        mapping(string => mapping(string => uint256)) limitInfo;
+        mapping(string => uint256) appliedLimit;
+        mapping(string => uint256) grantedLimit;
     }
 
-    mapping(bytes32 => Bank) bankInfo;
+    mapping(address => Bank) bankInfo;
     mapping(address => mapping(bytes32 => mapping(string => uint256))) stakerInfo;
 
     function register(
-        bytes calldata bankName,
+        address bankAdmin,
         bytes32 routingNumber,
         bytes calldata bankAddress,
         bytes calldata url
     ) public {
-
+        bankInfo[bankAdmin].routingNumber = routingNumber;
+        bankInfo[bankAdmin].bankAddress = bankAddress;
+        bankInfo[bankAdmin].url = url;
     }
+
+    function stakeForBank(address bank, string calldata currency, uint256 amount) 
 }
