406b4d4d37bc88d2bc84f25d9404573cb1eba6fc
rayedsikder <rayedsikder33@gmail.com>
2022-11-01 04:26:39 +0600

    Add checker for whitelisting addresses

--- contracts/UserRegistry.sol
+++ contracts/UserRegistry.sol
@@ -1,9 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
+import "./StakingLimit.sol";
+
 contract UserRegistry {
     mapping (address => bool) whitelist;
     
+    modifier onlyBank {
+        require(StakingLimit(getStakingLimitAddress()).bankInfo[msg.sender].isRegistered, "UserRegistry: Caller not bank admin");
+        _;
+    }
     function getWhitelistStatus(address user) public view returns (bool) {
         return whitelist[user];
     }
