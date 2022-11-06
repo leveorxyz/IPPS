be623ca67c3f3d4d32e2344bf6072adcf549d097
rayedsikder <rayedsikder33@gmail.com>
2022-11-06 07:45:35 +0600

    Add functions for account status for merchant and account holder

--- contracts/UserRegistry.sol
+++ contracts/UserRegistry.sol
@@ -2,19 +2,29 @@
 pragma solidity 0.8.17;
 
 import "./StakingLimit.sol";
+import "./ContractRegistry.sol";
 
 contract UserRegistry {
-    mapping (address => bool) whitelist;
+    mapping (address => bool) bankAccountHolder;
+    mapping (address => bool) merchant;
     
     modifier onlyBank {
-        require(StakingLimit(getStakingLimitAddress()).bankInfo[msg.sender].isRegistered, "UserRegistry: Caller not bank admin");
+        require(StakingLimit(ContractRegistry(STAKING_LIMIT())).bankInfo[msg.sender].isRegistered, "UserRegistry: Caller not bank admin");
         _;
     }
-    function getWhitelistStatus(address user) public view returns (bool) {
-        return whitelist[user];
+
+    function getBankAccountHolderStatus(address user) public view returns (bool) {
+        return bankAccountHolder[user];
     }
 
-    function whitelistUser(address user) public {
-        whitelist[user] = true;
+    function getMerchantStatus(address user) public view returns (bool) {
+        return merchant[user];
+    }
+    function whitelistBankAccountHolder(address user) public {
+        bankAccountHolder[user] = true;
+    }
+
+    function whitelistMerchant(address user) public {
+        merchant[user] = true;
     }
 }
