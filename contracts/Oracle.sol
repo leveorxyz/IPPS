c600aea17ea1499f26f66d0eb4cd7ff928bfe5eb
rayedsikder <rayedsikder33@gmail.com>
2022-11-14 19:02:24 +0600

    Add Oracle implementations

--- contracts/Oracle.sol
+++ contracts/Oracle.sol
@@ -1,6 +1,17 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.17;
 
+import "./TokenFactory.sol";
+import "./chainlink/PriceConsumerV3.sol";
+
 contract Oracle {
-    function getAssetPriceInUSD(address token) public view returns (uint256) {}
+    address contractRegistry;
+
+    function initialize(address _contractRegistry) public {
+        contractRegistry = _contractRegistry;
+    }
+
+    function getAssetPriceInUSD(string calldata currency) public view returns (int256) {
+        return PriceConsumerV3(ContractRegistry(contractRegistry).PRICE_CONSUMER_V3(currency)).getLatestPrice();
+    }
 }
