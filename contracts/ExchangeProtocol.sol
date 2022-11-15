3e9fba4aa221bfe2a860a8fa44c9ceab412b04a4
rayedsikder <rayedsikder33@gmail.com>
2022-11-15 13:28:56 +0600

    Add mint function in exchange protocol

--- contracts/ExchangeProtocol.sol
+++ contracts/ExchangeProtocol.sol
@@ -7,13 +7,39 @@ import "./UserRegistry.sol";
 import "./Oracle.sol";
 import "./interfaces/IUserData.sol";
 
-contract ExchangeProtocol {    
+contract ExchangeProtocol {
     address contractRegistry;
     uint256 feePercent = 1000000;
     uint256 immutable percentDivider = 100000000;
 
     function initialize(address _contractRegistry) public {
         contractRegistry = _contractRegistry;
+    }
+
+    function mintTokenToAccount(
+        address to,
+        string calldata currency,
+        uint256 amount
+    ) public {
+        UserRegistry registry = UserRegistry(
+            ContractRegistry(contractRegistry).USER_REGISTRY()
+        );
+        IUserData.UserType callerStatus = registry.getUserStatus(msg.sender);
+
+        require(
+            callerStatus == IUserData.UserType.BANK,
+            "ExchangeProtocol: The caller is not bank"
+        );
+        address tokenFactory = ContractRegistry(contractRegistry)
+            .TOKEN_FACTORY();
+        address token = TokenFactory(tokenFactory).getToken(currency);
+        require(
+            Token(token).totalSupply() + amount <=
+                StakingLimit(ContractRegistry(contractRegistry).STAKING_LIMIT())
+                    .getBankGrantedLimit(msg.sender, currency),
+            "ExchangeProtocol: The bank cannot mint more that its limit"
+        );
+        Token(token).mint(to, amount);
     }
 
     function transferToken(
@@ -45,8 +71,11 @@ contract ExchangeProtocol {
             require(receiverStatus == IUserData.UserType.BANK);
         }
 
-        require(srcToken != address(0) && dstToken != address(0), "ExchangeProtocol: Token doesn't exist");
-        
+        require(
+            srcToken != address(0) && dstToken != address(0),
+            "ExchangeProtocol: Token doesn't exist"
+        );
+
         uint256 feeAmount = receiverStatus == IUserData.UserType.MERCHANT
             ? (dstAmount * feePercent) / percentDivider
             : 0;
@@ -67,15 +96,13 @@ contract ExchangeProtocol {
                 );
             }
         } else {
-            int256 srcValue = Oracle(oracle).getAssetPriceInUSD(
-                srcToken
-            );
-            int256 dstValue = Oracle(oracle).getAssetPriceInUSD(
-                dstToken
-            );
+            int256 srcValue = Oracle(oracle).getAssetPriceInUSD(srcToken);
+            int256 dstValue = Oracle(oracle).getAssetPriceInUSD(dstToken);
             int256 tempDstAmount = int256(dstAmount);
             int256 srcAmount = (tempDstAmount * dstValue) / srcValue;
-            uint256 uSrcAmount = srcAmount < 0 ? uint256(-srcAmount) : uint256(srcAmount);
+            uint256 uSrcAmount = srcAmount < 0
+                ? uint256(-srcAmount)
+                : uint256(srcAmount);
             Token(srcToken).burnFrom(msg.sender, uSrcAmount);
             if (feeAmount != 0) {
                 uint256 dstAmountAfterFeeDeduction = dstAmount - feeAmount;
