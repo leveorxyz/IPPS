afe616c0057952776fa21315521182d3958b510b
rayedsikder <rayedsikder33@gmail.com>
2022-11-25 20:43:13 +0600

    Add protocol controlled mint-burn mechanism

--- contracts/ExchangeProtocol.sol
+++ contracts/ExchangeProtocol.sol
@@ -2,20 +2,37 @@
 pragma solidity 0.8.17;
 
 import "./TokenFactory.sol";
+import "./Token.sol";
 import "./UserRegistry.sol";
 import "./Oracle.sol";
 
 contract ExchangeProtocol {
     address userRegistry;
     address oracleAdddress;
-    
-    modifier isWhitelisted() {
-        require(UserRegistry(userRegistry).getWhitelistStatus(msg.sender), "ExchangeProtocol: User not whitelisted");
+
+    modifier isWhitelisted(address dstReceiver) {
+        require(
+            UserRegistry(userRegistry).getWhitelistStatus(msg.sender) &&
+                UserRegistry(userRegistry).getWhitelistStatus(dstReceiver),
+            "ExchangeProtocol: User not whitelisted"
+        );
         _;
     }
 
-    function transferToken(address srcToken, address dstToken, address dstReceiver, uint256 dstAmount) public isWhitelisted {
+    function transferToken(
+        address srcToken,
+        address dstToken,
+        address dstReceiver,
+        uint256 dstAmount
+    ) public isWhitelisted(dstReceiver) {
         uint256 srcValue = Oracle(oracleAdddress).getAssetPriceInUSD(srcToken);
         uint256 dstValue = Oracle(oracleAdddress).getAssetPriceInUSD(dstToken);
+        uint256 srcAmount = (dstAmount * dstValue) / srcValue;
+        require(
+            IERC20(srcToken).balanceOf(msg.sender) >= srcAmount,
+            "ExchangeProtocol: Not enough balance"
+        );
+        Token(srcToken).burnFrom(msg.sender, srcAmount);
+        Token(dstToken).mint(dstReceiver, dstAmount);
     }
 }
