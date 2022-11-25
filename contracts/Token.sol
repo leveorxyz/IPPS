11c9d4751f9632b410b8c2c53894948b1fd689fd
rayedsikder <rayedsikder33@gmail.com>
2022-11-25 14:36:55 +0600

    Add transfer and access mechanisms in Token

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -6,12 +6,29 @@ import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Token is ERC20, ERC20Burnable, Ownable {
-    constructor(string memory name_, string memory symbol_, address exchangeProtocol_)
-        ERC20(name_, symbol_)
-    {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        address exchangeProtocol_
+    ) ERC20(name_, symbol_) {
         _transferOwnership(exchangeProtocol_);
     }
+
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) public override onlyOwner returns (bool) {
+        _transfer(from, to, amount);
+        return true;
+    }    
+
+    function burn(uint256 amount) public pure override {
+        revert("Token: Operation not allowed");
+    }
+
 }
