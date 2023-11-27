c4f83a315870ed7e9366ec3fdee8f67992dcc6fd
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-28 05:51:23 +0600

    Add sign up and login page

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,11 +1,39 @@
-import type { NextPage } from 'next';
-import { Container } from '@chakra-ui/react';
-import Apply from '../components/Apply/Apply';
+import type { NextPage } from "next";
+import {
+  AbsoluteCenter,
+  Box,
+  Button,
+  Center,
+  Container,
+} from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
     <Container maxW="container.xl" py={10}>
-      <Apply />
+      <Box position="relative" h="100px">
+        <AbsoluteCenter mt="2">
+          <Button
+            variant="outline"
+            borderColor="blue"
+            size="sm"
+            textColor="teal"
+          >
+            Sign Up
+          </Button>
+        </AbsoluteCenter>
+      </Box>
+      <Box position="relative" h="100px">
+        <AbsoluteCenter mt="2">
+          <Button
+            variant="outline"
+            borderColor="blue"
+            size="sm"
+            textColor="teal"
+          >
+            Log In
+          </Button>
+        </AbsoluteCenter>
+      </Box>
     </Container>
   );
 };
