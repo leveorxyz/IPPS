fe352712bff8f6a39f4a5f13fd615fd702d627ae
Mahmudul Alam <mahmudul@ccprotocol.xyz>
2023-11-28 00:30:53 +0600

    feat: merchant create payment

---
+++ frontend/pages/merchant-create-payment.tsx
@@ -0,0 +1,51 @@
+import type { NextPage } from 'next';
+import {
+  Container,
+  Flex,
+  Stack,
+  Text,
+  Button,
+  Select,
+  FormControl,
+  FormLabel,
+  Input,
+  Box,
+} from '@chakra-ui/react';
+
+const MerchantCreatePayment: NextPage = () => {
+  return (
+    <Container maxW="container.xl" py={10}>
+      <Flex
+        fontSize="lg"
+        direction={{ base: 'column', md: 'row' }}
+        gap={10}
+        justifyContent="center"
+        w="full"
+      >
+        <Stack gap={5} minW="400px">
+          <FormControl>
+            <FormLabel>Pay currency</FormLabel>
+            <Select>
+              <option value="option1">Option 1</option>
+              <option value="option2">Option 2</option>
+            </Select>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Amount</FormLabel>
+            <Input type="number" placeholder="Enter amount" />
+          </FormControl>
+          <Box>
+            <Text>Converted amount: 10 USD</Text>
+            <Text>Current Balance: 10 USD</Text>
+            <Text>Third party fee amount: 0.01 USD</Text>
+          </Box>
+          <Button variant="outline" px="20">
+            Create payment link
+          </Button>
+        </Stack>
+      </Flex>
+    </Container>
+  );
+};
+
+export default MerchantCreatePayment;
