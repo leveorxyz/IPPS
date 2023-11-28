bd58da58fc8b902ebf4879d7119c1a76abda8663
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-29 01:36:11 +0600

    Hide menu link if not logged in

--- packages/nextjs/components/Layout/Header.tsx
+++ packages/nextjs/components/Layout/Header.tsx
@@ -5,10 +5,13 @@ import { ConnectButton } from '@rainbow-me/rainbowkit';
 import MobileDrawer from './MobileDrawer';
 import Logo from '../Logo/Logo';
 import MenuLink from '../MenuLink/MenuLink';
+import { useGlobalState } from '~~/services/store/store';
 
 const Header = () => {
   const [drawerOpen, setDrawerOpen] = useBoolean(false);
-
+  const userType = useGlobalState(state => state.userType);
+  console.log({userType});
+  
   return (
     <Box py={4}>
       <Container maxW="container.xl">
@@ -17,16 +20,21 @@ const Header = () => {
             <Link href="/">
               <Logo />
             </Link>
-
+            
             <HStack gap={7} display={{ base: 'none', md: 'flex' }}>
+            {userType === "Bank" && (
+              <>
               <MenuLink link="/" text="home" />
               <MenuLink link="/enlist" text="apply for enlistment" />
               <MenuLink link="/bank" text="bank" />
               <MenuLink link="/staker" text="staker info" />
               <MenuLink link="/earnings" text="My earnings" />
+              </>
+              )
+            }
               <ConnectButton label="Connect Wallet" />
             </HStack>
-
+            
             <Button variant="link" onClick={setDrawerOpen.on} display={{ md: 'none' }}>
               <IoMenu size="30" />
             </Button>
