c9113d4326e4c5e8b745b0eb9ff1660e8f9eab64
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-28 07:50:40 +0600

    Fix wagmi upgrade issue

--- packages/nextjs/hooks/index.ts
+++ packages/nextjs/hooks/index.ts
@@ -1,23 +1,21 @@
-import { useContract, useSigner } from 'wagmi';
+import { getContract } from 'wagmi/actions'
 import stakingLimitABI from '../abi/StakingLimit.json';
 import demoABI from '../abi/TestABI.json';
 
 export const useStakingLimitContract = () => {
-  const { data: signer } = useSigner();
+  // const { data: signer } = useSigner();
 
-  return useContract({
+  return getContract({
     address: '0x77B2376CE1bC580BAc3181a94Afab45a6bCad99d',
-    abi: stakingLimitABI,
-    signerOrProvider: signer,
+    abi: stakingLimitABI
   });
 };
 
 export const useDemoContract = () => {
-  const { data: signer } = useSigner();
+  // const { data: signer } = useSigner();
 
-  return useContract({
+  return getContract({
     address: '0xC4a02607C93d5c42Ce45317d7B4025fC5a3dda0e',
-    abi: demoABI,
-    signerOrProvider: signer,
+    abi: demoABI
   });
 };
