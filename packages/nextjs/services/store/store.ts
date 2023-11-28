5c1c60839c4b1650c7eb35452b6cd2b6351cb635
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-29 01:26:53 +0600

    Store user type

--- packages/nextjs/services/store/store.ts
+++ packages/nextjs/services/store/store.ts
@@ -10,11 +10,15 @@ import create from "zustand";
  */
 
 type TGlobalState = {
+  userType: "User" | "Staker" | "Merchant" | "Bank" | "Undefined",
+  setUserType: (newVal: "User" | "Staker" | "Merchant" | "Bank") => void;
   nativeCurrencyPrice: number;
   setNativeCurrencyPrice: (newNativeCurrencyPriceState: number) => void;
 };
 
 export const useGlobalState = create<TGlobalState>(set => ({
+  userType: "Undefined",
+  setUserType: (newVal: "User" | "Staker" | "Merchant" | "Bank"): void => set(() => ({ userType: newVal })),
   nativeCurrencyPrice: 0,
   setNativeCurrencyPrice: (newValue: number): void => set(() => ({ nativeCurrencyPrice: newValue })),
 }));
