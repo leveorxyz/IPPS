e50ef09d8ec045ddb61172e9c8da430e4a2b56c1
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-29 16:56:33 +0600

    Add customer merchant payment page

---
+++ packages/nextjs/pages/customer-mech-pay.tsx
@@ -0,0 +1,15 @@
+import { Container, Flex, Heading, Text } from "@chakra-ui/react"
+
+const CustomerMerchPay = () => {
+    return (
+        <Container maxW="container.xl" py={10}>
+            <Flex>
+                <Heading>
+                    Merchant Payment 
+                </Heading>
+            </Flex>
+        </Container>
+    )
+}
+
+export default CustomerMerchPay;
