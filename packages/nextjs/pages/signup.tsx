cc0b43ae50a979ccc60a5f18b20456190bf5a140
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-30 02:12:39 +0600

    Show tx link

--- packages/nextjs/pages/signup.tsx
+++ packages/nextjs/pages/signup.tsx
@@ -1,5 +1,5 @@
-import dynamic from 'next/dynamic'
-import type { NextPage } from 'next';
+import dynamic from "next/dynamic";
+import type { NextPage } from "next";
 import {
   Container,
   Flex,
@@ -11,58 +11,66 @@ import {
   Input,
   Select,
   Link,
-} from '@chakra-ui/react';
+  useToast,
+  Box,
+} from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
-import externalContracts from '~~/contracts/externalContracts';
-import { SetStateAction, useState } from 'react';
-import { useAccount, useContractWrite } from 'wagmi';
-import { userType } from '~~/types/usertype';
+import externalContracts from "~~/contracts/externalContracts";
+import { SetStateAction, useEffect, useState } from "react";
+import { useAccount, useContractWrite } from "wagmi";
+import { userType } from "~~/types/usertype";
 
-const WalletConnectInfo = dynamic(() => import('../components/WalletConnectInfo/WalletConnectInfo'), {
-  ssr: false, 
+const WalletConnectInfo = dynamic(() => import("../components/WalletConnectInfo/WalletConnectInfo"), {
+  ssr: false,
 });
 
 const SignUp: NextPage = () => {
   const router = useRouter();
   const { address } = useAccount();
+  const toast = useToast();
 
-  const [value, setValue] = useState("customer")
+  const [value, setValue] = useState("customer");
 
-  const { data, isLoading, isSuccess, write } = useContractWrite({
+  const { data, isLoading, isSuccess, writeAsync } = useContractWrite({
     address: externalContracts.UserRegistry.address,
     abi: externalContracts.UserRegistry.abi,
-    functionName: 'setUserStatus'
-  })
+    functionName: "setUserStatus",
+  });
 
-  const handleChange = (e: { target: { value: SetStateAction<string>; }; }) => {
+  const handleChange = (e: { target: { value: SetStateAction<string> } }) => {
     setValue(e.target.value);
+  };
+
+  useEffect(() => {
+    if(isSuccess){
+    toast({
+      render: () => (
+        <Box color="white" p={3} bg="blue.500">
+          <Link className="underline" target="_blank" href={"https://sepolia.etherscan.io/tx/" + data?.hash}>
+            Tx Link
+          </Link>
+        </Box>
+      ),
+      status: "success",
+      duration: 10000,
+      isClosable: true,
+    });
   }
+  }, [isSuccess]);
 
   async function signUp() {
-    console.log("Signing up...")
-    await write({
-      args: [address, userType(value)]
-    })
+    await writeAsync({
+      args: [address, userType(value)],
+    });
     // router.push("/login")
   }
 
   return (
     <Container maxW="container.xl" py={10}>
-      <Flex
-        fontSize="lg"
-        direction={{ base: 'column', md: 'row' }}
-        gap={10}
-        justifyContent="space-around"
-      >
+      <Flex fontSize="lg" direction={{ base: "column", md: "row" }} gap={10} justifyContent="space-around">
         <Stack minW="400px" gap={10}>
-          <Text
-            color="white"
-            fontSize="lg"
-            textTransform="uppercase"
-            textAlign="center"
-            fontWeight="bold"
-          >
+          <Text color="white" fontSize="lg" textTransform="uppercase" textAlign="center" fontWeight="bold">
             Sign up
           </Text>
 
@@ -72,30 +80,33 @@ const SignUp: NextPage = () => {
           </FormControl>
           <FormControl>
             <FormLabel>User Type</FormLabel>
-            <Select
-            onChange={handleChange} 
-            value={value}
-            >
+            <Select onChange={handleChange} value={value}>
               <option value="customer">Customer</option>
               <option value="merchant">Merchant</option>
               <option value="staker">Staker</option>
               <option value="bank">Bank</option>
             </Select>
           </FormControl>
-          <WalletConnectInfo/>
-          
-          <Button disabled={!write} variant="outline" px="20" onClick={signUp}>
+          <WalletConnectInfo />
+
+          <Button disabled={!writeAsync} variant="outline" px="20" onClick={signUp}>
             Verify wallet and sign up
           </Button>
           {isLoading && <div>Check Wallet</div>}
-          {isSuccess && <div>Transaction: {JSON.stringify(data)}</div>}
+
+          {isSuccess && (
+            <div className="text-green-700">
+              Transaction Submission successful
+             
+            </div>
+          )}
 
           <Text textTransform="uppercase" textAlign="center" mt="10">
-            Already have an account?{' '}
+            Already have an account?{" "}
             <Link href="/login">
-            <Button variant="link" textTransform="uppercase" fontSize="sm" color="white">
-              Login here{' '}
-            </Button>
+              <Button variant="link" textTransform="uppercase" fontSize="sm" color="white">
+                Login here{" "}
+              </Button>
             </Link>
           </Text>
         </Stack>
@@ -105,5 +116,3 @@ const SignUp: NextPage = () => {
 };
 
 export default SignUp;
-
-
