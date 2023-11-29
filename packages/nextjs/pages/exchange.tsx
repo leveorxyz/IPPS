02eafd072f8fd2cffc4a2cdfd515b60784423d28
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-29 16:52:58 +0600

    Add customer exchange page

---
+++ packages/nextjs/pages/exchange.tsx
@@ -0,0 +1,15 @@
+import { Container, Flex, Heading, Text } from "@chakra-ui/react"
+
+const Exchange = () => {
+    return (
+        <Container maxW="container.xl" py={10}>
+            <Flex>
+                <Heading>
+                    Exchange
+                </Heading>
+            </Flex>
+        </Container>
+    )
+}
+
+export default Exchange;
