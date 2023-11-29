2802c84ddb9813d4811a9974804eab45bb9b6270
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-29 20:13:17 +0600

    Update customer merchant payment page

--- packages/nextjs/pages/customer-mech-pay.tsx
+++ packages/nextjs/pages/customer-mech-pay.tsx
@@ -1,17 +1,29 @@
-import { Container, Heading, Stack, Text } from "@chakra-ui/react"
+import { Box, Container, FormControl, FormLabel, Heading, Select, Stack, Text } from "@chakra-ui/react";
 import Scanner from "~~/components/Mobile/Scanner";
 
 const CustomerMerchPay = () => {
-    return (
-        <Container maxW="container.xl" py={10}>
-            <Stack>
-                <Heading>
-                    Merchant Payment 
-                </Heading>
-                <Scanner/>
-            </Stack>
-        </Container>
-    )
-}
+  return (
+    <Container maxW="container.xl" py={10}>
+      <Stack>
+        <Heading>Merchant Payment</Heading>
+        <Scanner />
+        <Box mx={"auto"} mt={9}>
+          <FormControl>
+            <FormLabel>Choose currency</FormLabel>
+            <Select>
+              <option value="option1">USD</option>
+              <option value="option2">Euro</option>
+            </Select>
+          </FormControl>
+        </Box>
+        <Box mx={"auto"}>
+            <Text>Amount to pay: 10 USD</Text>
+            <Text>Current Balance: 10 USD</Text>
+            <Text>Third party fee amount: 0.01 USD</Text>
+          </Box>
+      </Stack>
+    </Container>
+  );
+};
 
 export default CustomerMerchPay;
