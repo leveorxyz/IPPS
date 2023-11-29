ec2b6b2b83349148b36ae864a9413de4dc5e3f69
Tahlil <bsse0803@iit.du.ac.bd>
2023-11-29 22:54:19 +0600

    Update deploy script

--- packages/hardhat/deploy/deploy.ts
+++ packages/hardhat/deploy/deploy.ts
@@ -1,35 +1,54 @@
 import { ethers } from "hardhat";
 import { ContractRegistry, TokenFactory, ExchangeProtocol, StakingLimit, RewardPool, Oracle, UserRegistry, TestUSDT, TestEURT } from "../typechain-types";
+import fs from 'fs';
+import path from 'path';
+
+
 
 async function main() {
+  const contractDetailsDataPath = path.join(__dirname, "../", "../", "nextjs", "contracts", "externalAddresses.json");
+  const jsonData = fs.readFileSync(contractDetailsDataPath, 'utf8');
+  const jsonObject = JSON.parse(jsonData);
 
   const ContractRegistry = await ethers.getContractFactory("ContractRegistry");
   const contractRegistry: ContractRegistry = await ContractRegistry.deploy();
   console.log(`ContractRegistry deployed to ${contractRegistry.address}`);
-
+  jsonObject.contractRegistry = contractRegistry.address;
   const TokenFactory = await ethers.getContractFactory("TokenFactory");
   const tokenFactory: TokenFactory = await TokenFactory.deploy();
   console.log(`TokenFactory deployed to ${tokenFactory.address}`);
+  jsonObject.tokenFactory = tokenFactory.address;
+
 
   const ExchangeProtocol = await ethers.getContractFactory("ExchangeProtocol");
   const exchangeProtocol: ExchangeProtocol = await ExchangeProtocol.deploy();
   console.log(`ExchangeProtocol deployed to ${exchangeProtocol.address}`);
+  jsonObject.exchangeProtocol = exchangeProtocol.address;
+
 
   const StakingLimit = await ethers.getContractFactory("StakingLimit");
   const stakingLimit: StakingLimit = await StakingLimit.deploy();
   console.log(`StakingLimit deployed to ${stakingLimit.address}`);
+  jsonObject.stakingLimit = stakingLimit.address;
+
 
   const RewardPool = await ethers.getContractFactory("RewardPool");
   const rewardPool: RewardPool = await RewardPool.deploy();
   console.log(`RewardPool deployed to ${rewardPool.address}`);
+  jsonObject.rewardPool = rewardPool.address;
+
 
   const Oracle = await ethers.getContractFactory("Oracle");
   const oracle: Oracle = await Oracle.deploy();
   console.log(`Oracle deployed to ${oracle.address}`);
+  jsonObject.oracle = rewardPool.oracle;
+
 
   const UserRegistry = await ethers.getContractFactory("UserRegistry");
   const userRegistry: UserRegistry = await UserRegistry.deploy();
   console.log(`UserRegistry deployed to ${userRegistry.address}`);
+  jsonObject.userRegistry = userRegistry.address;
+
 
   // Deploy test USDT contract - this will be used to simulate stablecoin USDT for testnet
 
@@ -39,7 +58,10 @@ async function main() {
 
   const TestEURT = await ethers.getContractFactory("TestEURT");
   const testEURT: TestUSDT = await TestEURT.deploy();
-  console.log(`TestUSDT deployed to ${testEURT.address}`);
+  console.log(`TestEURT deployed to ${testEURT.address}`);
+  jsonObject.testEURT = testEURT.address;
+  const updatedJsonData = JSON.stringify(jsonObject, null, 2);
+  fs.writeFileSync(contractDetailsDataPath, updatedJsonData, 'utf8');
 
   await contractRegistry.setExchangeProtocol(exchangeProtocol.address);
   await contractRegistry.setOracle(oracle.address);
